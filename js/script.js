41820edd3d4fe3a13f19f96fa71ffb96a8269c0e
Dmitry Skok <dimas.skok95@gmail.com>
2021-12-10 19:06:15 +0100

    Complete computer random

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-let computerMove = 'kamien';
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagralem ' + computerMove + '! Jesli Twoj ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+    computerMove = 'papier';
+} else if(randomNumber == 3){
+    computerMove = 'nozyce';
+} else {
+    console.log ('zle dzila random,bo liczba ' + randomNumber + 'poza zakresem od 1 do 3');
+}
+
+console.log('Computer wpisał: ' + computerMove);
