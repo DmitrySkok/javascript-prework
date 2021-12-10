39f356efe45d8a90358847a25d710f42a811bbb6
Dmitry Skok <dimas.skok95@gmail.com>
2021-12-10 23:56:15 +0100

    Add functions

--- js/script.js
+++ js/script.js
@@ -1,70 +1,124 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+console.log("Wylosowana liczba to: " + randomNumber);
 
-let computerMove = 'nieznany ruch';
+// let computerMove = 'nieznany ruch';
+let computerMove = getComputerName(randomNumber);
 
-if(randomNumber == 1){
-  computerMove = 'kamien';
-} else if(randomNumber == 2){
-    computerMove = 'papier';
-} else if(randomNumber == 3){
-    computerMove = 'nozyce';
-} else {
-    console.log ('zle dzila random,bo liczba ' + randomNumber + ' poza zakresem od 1 do 3');
+function getComputerName(argMoveId) {
+  if (argMoveId == 1) {
+    return "kamien";
+  } else if (argMoveId == 2) {
+    return "papier";
+  } else if (argMoveId == 3) {
+    return "nozyce";
+  }
+  console.log("Nie znam ruchu o id " + argMoveId + ".");
+  return "nieznany ruch";
 }
-
-console.log('Computer wpisał: ' + computerMove);
+console.log("Computer wpisał: " + computerMove);
 
 let playerInput = prompt('Wybierz swoj ruch! 1: kamien, 2: papier, 3: nozyce.');
+// let playerInput = 2;
+let playerMove = getPlayerMove(playerInput);
 
-let playerMove = 'nieznany ruch';
+function getPlayerMove(argMoveId) {
+  if (argMoveId == 1) {
+    return "kamien";
+  } else if (argMoveId == 2) {
+    return "papier";
+  } else if (argMoveId == 3) {
+    return "nozyce";
+  }
+//   console.log("Nie znam ruchu o id " + argMoveId + ".");
+  return "nieznany ruch";
+}
+console.log("Gracz wpisał: " + playerMove);
 
-if(playerInput == '1'){
-  playerMove = 'kamien';
-} else if(playerInput == '2'){
-    playerMove = 'papier';
-} else if(playerInput == '3'){
-    playerMove = 'nozyce';
-} else {
-    printMessage ('zle podales liczbe,bo liczba ' + playerInput + ' poza zakresem od 1 do 3');
+function displayResult(argComputerMove, argPlayerMove) {
+  if (
+    (argComputerMove == "kamien" && argPlayerMove == "papier") ||
+    (argComputerMove == "nozyce" && argPlayerMove == "kamien") ||
+    (argComputerMove == "papier" && argPlayerMove == "nozyce")
+  ) {
+    console.log("Human WIN");
+  } else if (
+    (argComputerMove == "papier" && argPlayerMove == "kamien") ||
+    (argComputerMove == "kamien" && argPlayerMove == "nozyce") ||
+    (argComputerMove == "nozyce" && argPlayerMove == "papier")
+  ) {
+    console.log("Robot WIN");
+  } else if (argComputerMove == argPlayerMove) {
+    console.log("Draw");
+  }
 }
 
-console.log('Gracz wpisał: ' + playerMove);
+getPlayerMove();
+displayResult(computerMove, playerMove);
+
+
+
+// // if(randomNumber == 1){
+// //   computerMove = 'kamien';
+// // } else if(randomNumber == 2){
+// //     computerMove = 'papier';
+// // } else if(randomNumber == 3){
+// //     computerMove = 'nozyce';
+// // } else {
+// //     console.log ('zle dzila random,bo liczba ' + randomNumber + ' poza zakresem od 1 do 3');
+// // }
+
+// console.log('Computer wpisał: ' + computerMove);
+
+// // let playerInput = prompt('Wybierz swoj ruch! 1: kamien, 2: papier, 3: nozyce.');
+
+// let playerMove = 'nieznany ruch';
+
+// if(playerInput == '1'){
+//   playerMove = 'kamien';
+// } else if(playerInput == '2'){
+//     playerMove = 'papier';
+// } else if(playerInput == '3'){
+//     playerMove = 'nozyce';
+// } else {
+//     console.log ('zle podales liczbe,bo liczba ' + playerInput + ' poza zakresem od 1 do 3');
+// }
+
+// console.log('Gracz wpisał: ' + playerMove);
 
 // kodillaCode__________
 
-// printMessage('Human chose: ' + playerMove);
-// printMessage('Robot chose: ' + computerMove);
+// console.log('Human chose: ' + playerMove);
+// console.log('Robot chose: ' + computerMove);
 
 // if(computerMove == 'kamien' && playerMove == 'papier'){
-//     printMessage('Human WIN');
+//     console.log('Human WIN');
 // } else if(computerMove == 'papier' && playerMove == 'kamien'){
-//     printMessage('Robot WIN');
+//     console.log('Robot WIN');
 // } else if(computerMove == 'nozyce' && playerMove == 'kamien'){
-//     printMessage('Human WIN');
+//     console.log('Human WIN');
 // } else if(computerMove == 'kamien' && playerMove == 'nozyce'){
-//     printMessage('Robot WIN');
+//     console.log('Robot WIN');
 // } else if(computerMove == 'papier' && playerMove == 'nozyce'){
-//     printMessage('Human WIN'); 
+//     console.log('Human WIN');
 // } else if(computerMove == 'nozyce' && playerMove == 'papier'){
-//     printMessage('Robot WIN');
+//     console.log('Robot WIN');
 // } else if(computerMove == playerMove){
-//     printMessage('Draw');
+//     console.log('Draw');
 // }
 
 // myCode_________
 
-printMessage('Human chose: ' + playerMove);
-printMessage('Robot chose: ' + computerMove);
+// console.log('Human chose: ' + playerMove);
+// console.log('Robot chose: ' + computerMove);
 
-if((computerMove == 'kamien' && playerMove == 'papier') || (computerMove == 'nozyce' && playerMove == 'kamien') || (computerMove == 'papier' && playerMove == 'nozyce')){
-    printMessage('Human WIN');
-} else if((computerMove == 'papier' && playerMove == 'kamien') || (computerMove == 'kamien' && playerMove == 'nozyce') || (computerMove == 'nozyce' && playerMove == 'papier')){
-    printMessage('Robot WIN');
-} else if(computerMove == playerMove){
-    printMessage('Draw');
-}
+// if((computerMove == 'kamien' && playerMove == 'papier') || (computerMove == 'nozyce' && playerMove == 'kamien') || (computerMove == 'papier' && playerMove == 'nozyce')){
+//     console.log('Human WIN');
+// } else if((computerMove == 'papier' && playerMove == 'kamien') || (computerMove == 'kamien' && playerMove == 'nozyce') || (computerMove == 'nozyce' && playerMove == 'papier')){
+//     console.log('Robot WIN');
+// } else if(computerMove == playerMove){
+//     console.log('Draw');
+// }
 
 // anotherOneCode
 
